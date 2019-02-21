eb86a8e2a45d07c648557d9b2e1730e329bfdcc3
Torphage <filip.torphage1@gmail.com>
2019-02-21 10:57:05 +0100

    get posts from db

--- app.rb
+++ app.rb
@@ -3,6 +3,11 @@ require 'slim'
 require 'bcrypt'
 require 'sqlite3'
 
-get('/index') do
-    slim(:index)
+get('/') do
+    db = SQLite3::Database.new("db/blog.db")
+    db.results_as_hash = true
+
+    result = db.execute("SELECT * FROM Posts")
+    
+    slim(:index, locals:{posts: result})
 end
